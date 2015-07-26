b35a558dd2b99429342df8c90901802ec5c1da05
Julia Bond <julia@yogaed.com>
2015-07-25 19:27:26 -1000

    Second tests written and passing.

--- chirp.js
+++ chirp.js
@@ -1,8 +1,15 @@
 module.exports = chirp;
 
+var chirping = [];
+
 function chirp(n){
   if (typeof n !== 'number'){
-    return "must pass a number through the function";
+    return "must pass a number through the second parameter";
   }
-
-};
+  if(n > 0){
+    chirping.push("chirp");
+    n--;
+    return chirp(n);
+  }
+  return chirping.join(" ");
+}
